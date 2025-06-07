215e65e554f9ff1e151d20f6ffec535a8f74460c
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-07 12:49:01 +0900

    added dict to strcut

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -1,10 +1,21 @@
 pub const MIN_NB_WORDS: usize = 12;
 pub const MAX_NB_WORDS: usize = 33;
+pub const MAX_WORDS_DICT: usize = 2048;
 
 const EOF: u16 = u16::max_value();
 
-#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "zeroize", derive(Zeroize, ZeroizeOnDrop))]
-pub struct Mnemonic {
+pub struct Mnemonic<'a> {
+    lang_words: [&'a str; MAX_WORDS_DICT],
     indicators: [u16; MAX_NB_WORDS],
+}
+
+impl<'a> Mnemonic<'a> {
+    pub fn from(dictionary: [&'a str; MAX_WORDS_DICT]) -> Self {
+        Self {
+            lang_words: dictionary,
+            indicators: [0u16; MAX_NB_WORDS],
+        }
+    }
 }
