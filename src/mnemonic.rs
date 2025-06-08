c8986abf42151cc26d74ab527d042763c23a4486
Rinat <rinat@Rinats-MacBook-Air.local>
2025-06-08 12:50:47 +0900

    added fmt

--- src/mnemonic.rs
+++ src/mnemonic.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 pub const MIN_NB_WORDS: usize = 12;
 pub const MAX_NB_WORDS: usize = 33;
 pub const MAX_WORDS_DICT: usize = 2048;
@@ -27,6 +29,18 @@ impl<'a, 'b> Iterator for MnemonicIter<'a, 'b> {
         let word_index = self.mnemonic.indicators[self.position] as usize;
         self.position += 1;
         Some(self.mnemonic.lang_words[word_index])
+    }
+}
+
+impl<'a> fmt::Display for Mnemonic<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, word) in self.iter().enumerate() {
+            if i > 0 {
+                f.write_str(" ")?;
+            }
+            f.write_str(word)?;
+        }
+        Ok(())
     }
 }
 
